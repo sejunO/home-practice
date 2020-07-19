183d892ff3ee3169f2c29ec34b1c7efe35d2657d
sejunO <frame8916@gmail.com>
2020-07-19 18:25:52 +0900

    javascript 공부

---
+++ src/main/java/webapp/nomad.js
@@ -0,0 +1,36 @@
+console.log('I\'m Working. IM JS.');
+ 
+
+//Variable
+
+let a = 221;
+let b = a - 5;
+a = 4;
+console.log(b,a); // 216 4 
+// 변수는 기본적으로 cosnt, 필요할 때만 let
+// camel표기법의 시작은 소문자
+
+//Array
+
+const mon = "Mon";
+const tue = "Tue";
+const wed = "Wed";
+const thu = "thu";
+const fri = "Fri";
+console.log(mon,tue,wed,thu,fri);
+const something = "Something";
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", 54, true, something];
+console.log(daysOfWeek);
+console.log(daysOfWeek[2]);
+
+
+//Object
+
+const junInfo =  {
+    name: "sejun",
+    age: 27,
+    gender: "Male"
+}
+console.log(junInfo);
+console.log(junInfo.gender);
+
