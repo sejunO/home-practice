5d212e10a09b91d35b81a0ed9370c099dc58a484
sejunO <frame8916@gmail.com>
2020-08-22 19:05:27 +0900

    study

--- src/main/java/webapp/practice2.js
+++ src/main/java/webapp/practice2.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function print() {
     console.log('Hello');
 }
@@ -31,3 +33,41 @@ function printMessage() {
 }
 
 printMessage();
+
+
+function printOb(object) {
+    console.log(object.name, object.age);
+}
+
+const se = { 
+    name: 'sejun', 
+    age: 4
+ };
+printOb(se)
+
+
+function makePerson(name, age) {
+    return {
+      name,
+      age,
+    };
+  }
+
+const person = makePerson('sejun', 27);
+console.log(person);
+
+const user = { name: 'sejun', age: 27};
+const user2 = user;
+
+console.log(user.name);
+const user3 = Object.assign({}, user);
+console.clear();
+
+user2.name = 'jun';
+console.log(user3);
+console.log(user == user2);
+
+const f1 = {color: 'red'};
+const f2 = {color: 'blue', size: 'big'};
+const mixed = Object.assign({}, f1, f2);
+console.log(mixed);
