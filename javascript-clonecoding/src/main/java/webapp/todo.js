fe7d917155871a5b116552f1035a0cace134941b
sejunO <frame8916@gmail.com>
2020-08-26 20:28:28 +0900

    .

--- javascript-clonecoding/src/main/java/webapp/todo.js
+++ javascript-clonecoding/src/main/java/webapp/todo.js
@@ -1,2 +1,28 @@
-const form = document.querySelector(".js-toDoForm"),
-    input  = form.querySelector("input");
+const toDoform = document.querySelector(".js-toDoForm"),
+    toDoInput  = form.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+function loadToDos() {
+    const toDos = localStorage.getItem(TODOS_LS);
+    if (toDos !== null) {
+
+    }
+}
+
+function paintToDo(text) {
+
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDInput.value;
+    paintToDo(turrentvalue);
+}
+function init() {
+    loadToDos();
+    toDoform.addEventListener("submit",handleSubmit);
+}
+
+init();
